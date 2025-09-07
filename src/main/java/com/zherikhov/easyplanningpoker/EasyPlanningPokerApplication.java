ed5f6eebe357d9d0417d4d2c7988fe943587ee0f
Zherikhov <39011402+Zherikhov@users.noreply.github.com>
2025-09-07 10:43:40 +0200

    init

---
+++ src/main/java/com/zherikhov/easyplanningpoker/EasyPlanningPokerApplication.java
@@ -0,0 +1,13 @@
+package com.zherikhov.easyplanningpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPlanningPokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPlanningPokerApplication.class, args);
+    }
+
+}
