2fc1406842f4c4f88ede2d33c0c2449bd046ee9c
Zherikhov <39011402+Zherikhov@users.noreply.github.com>
2026-02-22 18:17:36 +0100

    feat(logging): add HTTP request logging and improve logging configuration
    Enabled request logging via `CommonsRequestLoggingFilter` and made updates to `application.properties` including optional DB logging configurations.

--- src/main/java/com/easysprintpoker/EasyPlanningPokerApplication.java
+++ src/main/java/com/easysprintpoker/EasyPlanningPokerApplication.java
@@ -2,11 +2,23 @@ package com.easysprintpoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class EasyPlanningPokerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyPlanningPokerApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(false);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
     }
 }
